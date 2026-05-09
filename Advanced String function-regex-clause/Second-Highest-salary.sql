c92f833a69111d72897830298bbe806b7b945624
Mahabub Hasan Mamun <102401671+mahabubmamun@users.noreply.github.com>
2026-05-09 18:00:29 +0600

    Create Second-Highest-salary.sql

---
+++ Advanced String function-regex-clause/Second-Highest-salary.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+# First Approach
+-- SELECT 
+-- (
+--     SELECT DISTINCT salary
+--     FROM Employee
+--     ORDER BY salary DESC
+--     LIMIT 1 OFFSET 1
+-- ) 
+-- AS SecondHighestSalary
+
+# Second Approach
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
